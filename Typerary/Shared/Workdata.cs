1816acc0129afbe768703474a97e455ec945288f
Arthur-Lugh <38004439+WhiteFox-Lugh@users.noreply.github.com>
2022-09-17 12:34:25 +0900

    feat: works page

---
+++ Typerary/Shared/Workdata.cs
@@ -0,0 +1,24 @@
+﻿namespace Typerary.Shared
+{
+    public class WorkMetadata
+    {
+        public List<WorkData> WorkList { get; set; }
+    }
+
+    public class WorkData
+    {
+        public string WorkId { get; set; }
+
+        public string Title { get; set; }
+
+        public string TitleYomi { get; set; }
+
+        public string? SubTitle { get; set; }
+
+        public string Author { get; set; }
+
+        public string AuthorYomi { get; set; }
+
+        public Uri WorkUri { get; set; }
+    }
+}
