2c517d4234b486ef81ea16276bd2a9fb7ca46ac8
Arthur-Lugh <38004439+WhiteFox-Lugh@users.noreply.github.com>
2022-09-19 05:36:26 +0900

    wip: release-note

---
+++ Typerary/Shared/ReleaseHistory.cs
@@ -0,0 +1,23 @@
+﻿namespace Typerary.Shared
+{
+    public class ReleaseHistoryMetadata
+    {
+        public List<ReleaseHistory> Histories { get; set; }
+    }
+
+    public class ReleaseHistory
+    {
+        public DateTime GetUpdateDateTime()
+        {
+            if (!DateTime.TryParse(UpdateDate, out var res))
+            {
+                throw new InvalidDataException("Error: invalid datetime format in release history");
+            }
+            return res;
+        }
+
+        public string UpdateDate { get; set; }
+        public string Version { get; set; }
+        public string DetailText { get; set; }
+    }
+}
