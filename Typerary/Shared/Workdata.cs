9635692526b7e0b62e3c3728cb6938636772c047
Arthur-Lugh <38004439+WhiteFox-Lugh@users.noreply.github.com>
2022-09-18 15:04:54 +0900

    fix: works page layout

--- Typerary/Shared/Workdata.cs
+++ Typerary/Shared/Workdata.cs
@@ -7,6 +7,15 @@
 
     public class WorkData
     {
+        private const string DefaultScreenName = "---";
+        private const Radzen.BadgeStyle DefaultBadgeStyle = Radzen.BadgeStyle.Dark;
+        private static readonly Dictionary<string, (string screenName, Radzen.BadgeStyle badgeStyle)> langToDisplayString = new(){
+            {"Japanese", ("日本語", Radzen.BadgeStyle.Info)}, {"English", ("英語", Radzen.BadgeStyle.Danger)}
+        };
+        public string GetLanguageScreenName() => langToDisplayString.ContainsKey(Language) ? langToDisplayString[Language].screenName : DefaultScreenName;
+        public Radzen.BadgeStyle GetLanguageBadgeStyle() => langToDisplayString.ContainsKey(Language) ? langToDisplayString[Language].badgeStyle : DefaultBadgeStyle;
+
+
         public string WorkId { get; set; }
 
         public string Title { get; set; }
@@ -17,7 +26,11 @@
 
         public string Author { get; set; }
 
-        public string AuthorYomi { get; set; }
+        public string? AuthorYomi { get; set; }
+
+        public string Language { get; set; }
+
+        public string Source { get; set; }
 
         public Uri WorkUri { get; set; }
     }
