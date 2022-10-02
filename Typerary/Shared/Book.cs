e15ea224afa740b689a429fcfba7d96f98710b0d
Arthur-Lugh <38004439+WhiteFox-Lugh@users.noreply.github.com>
2022-10-02 15:57:15 +0900

    feat: load json data

---
+++ Typerary/Shared/Book.cs
@@ -0,0 +1,20 @@
+﻿namespace Typerary.Shared
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public BookData[] Content { get; set; }
+    }
+
+    public class BookData
+    {
+        public string Section { get; set; }
+        public Sentence[] Sentences { get; set; }
+    }
+
+    public class Sentence
+    {
+        public string OriginSentence { get; set; }
+        public string JudgeSentence { get; set; }
+    }
+}
