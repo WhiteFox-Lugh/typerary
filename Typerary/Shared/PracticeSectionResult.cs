62323a410284f653c299f94dc26ccc1acd1c0d0d
WhiteFox-Lugh <38004439+WhiteFox-Lugh@users.noreply.github.com>
2022-12-21 09:19:58 +0900

    dmp の diff をオリジナルの diff に変換

--- Typerary/Shared/PracticeSectionResult.cs
+++ Typerary/Shared/PracticeSectionResult.cs
@@ -7,10 +7,31 @@ namespace Typerary.Shared
 
     public class PracticeSectionResult
     {
+        public enum DiffOperation
+        {
+            Equal,
+            Insert,
+            Delete,
+            Replace
+        }
+        public class TyperaryDiff
+        {
+            public readonly DiffOperation operation;
+            public readonly ValueString input;
+            public readonly ValueString? correctString;
+
+            public TyperaryDiff(DiffOperation op, ValueString inputText, ValueString? correct = null)
+            {
+                operation = op;
+                input = inputText;
+                correctString = correct;
+            }
+        }
+
         private static diff_match_patch dmp;
         private readonly Func<ValueString, string> insertHtmlStyle = (ValueString text) => $"<ins style=\"background:#e6ffe6; font-weight:bold\">{text}</ins>";
         private readonly Func<ValueString, string> deleteHtmlStyle = (ValueString text) => $"<del style=\"background:#ffe6e6; font-weight:bold\">{text}</del>";
-        private readonly Func<ValueString, ValueString, string> replaceHtmlStyle = (ValueString wrongText, ValueString collectText) => $"<span style=\"background:#e6e6ff; font-weight:bold\"><del>{wrongText}</del> &rarr; {collectText}</span>";
+        private readonly Func<ValueString, ValueString?, string> replaceHtmlStyle = (ValueString wrongText, ValueString? collectText) => $"<span style=\"background:#e6e6ff; font-weight:bold\"><del>{wrongText}</del> &rarr; {collectText}</span>";
         private readonly Func<ValueString, string> equalHtmlStyle = (ValueString text) => $"<span style=\"color:#666666\">{text}</span>";
         public int CollectCount { get; private set; } = 10;
         public int WrongCount { get; private set; } = 1;
@@ -28,27 +49,41 @@ namespace Typerary.Shared
 
         private List<Diff> GenerateDiff() => dmp.diff_main(InputSentence, JudgeSentence);
 
-        private string ConvertDiffsToHtmlString(List<Diff> diffs)
+        private List<TyperaryDiff> ConvertDiffToTyperaryDiff(List<Diff> diffs)
         {
-            var stringBuilder = ReseekableStringBuilder.AcquirePooledStringBuilder();
-            for (var i = 0; i < diffs.Count; i++)
+            var ret = new List<TyperaryDiff>();
+
+            for (var i = 0; i < diffs.Count; ++i)
             {
                 var ithOperation = diffs[i].operation;
                 var ithText = diffs[i].text;
                 var isReplace = (i + 1 < diffs.Count) && (diffs[i + 1].operation is Operation.INSERT);
-                stringBuilder.Append(
-                    ithOperation switch
-                    {
-                        Operation.EQUAL => equalHtmlStyle.Invoke(ithText),
-                        Operation.INSERT => insertHtmlStyle.Invoke(ithText),
-                        Operation.DELETE when isReplace => replaceHtmlStyle.Invoke(ithText, diffs[i + 1].text),
-                        Operation.DELETE => deleteHtmlStyle.Invoke(ithText),
-                        _ => ""
-                    });
-                if (isReplace)
+                ret.Add(ithOperation switch
                 {
-                    i++;
-                }
+                    Operation.EQUAL => new TyperaryDiff(DiffOperation.Equal, ithText),
+                    Operation.INSERT => new TyperaryDiff(DiffOperation.Insert, ithText),
+                    Operation.DELETE when isReplace => new TyperaryDiff(DiffOperation.Replace, ithText, diffs[i + 1].text),
+                    Operation.DELETE => new TyperaryDiff(DiffOperation.Delete, ithText),
+                    _ => throw new NotImplementedException("Fatal Error: Diff -> TyperaryDiff のパターン網羅漏れがあります"),
+                });
+            }
+            return ret;
+        }
+
+        private string ConvertDiffsToHtmlString(List<TyperaryDiff> diffs)
+        {
+            var stringBuilder = ReseekableStringBuilder.AcquirePooledStringBuilder();
+            foreach (var diff in diffs)
+            {
+                var htmlText = diff.operation switch
+                {
+                    DiffOperation.Equal => equalHtmlStyle.Invoke(diff.input),
+                    DiffOperation.Insert => insertHtmlStyle.Invoke(diff.input),
+                    DiffOperation.Delete => deleteHtmlStyle.Invoke(diff.input),
+                    DiffOperation.Replace => replaceHtmlStyle.Invoke(diff.input, diff.correctString),
+                    _ => ""
+                };
+                stringBuilder.Append(htmlText);
             }
 
             var text = stringBuilder.ToString();
@@ -61,11 +96,8 @@ namespace Typerary.Shared
         public void SetDiffMarkUpSentence()
         {
             var diffs = GenerateDiff();
-            foreach (var diff in diffs)
-            {
-                Console.WriteLine($"({diff.operation}, {diff.text}");
-            }
-            DiffMarkUpSentence = ConvertDiffsToHtmlString(diffs);
+            var convertedDiff = ConvertDiffToTyperaryDiff(diffs);
+            DiffMarkUpSentence = ConvertDiffsToHtmlString(convertedDiff);
         }
     }
 }
